91cdf5a0a05936efca70aba4a94e9214337e0ccf
MatthiasHe <matthiasaion@gmail.com>
2020-07-29 01:20:40 +0200

    Add status and season filters

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,19 @@
 <template>
 	<section class="columns is-multiline">
 		<div class="">
+			<label>Search by name:</label>
 			<input type="text" v-model="nameFilter" placeholder="Search by name.."/>
-			<label>Search by name :</label>
+
+			<label>Search by status:</label>
+			<select v-model="statusFilter">
+				<option v-for="status in statusesList" :value="status">{{ status ? status : 'All' }}</option>
+			</select>
+
+			<label>Search by season:</label>
+			<select v-model="seasonFilter">
+				<option :value="0">All seasons</option>
+				<option v-for="season in seasonsNumber" :value="season">{{ season }}</option>
+			</select>
 		</div>
 		<div class="card column is-one-third" v-for="character in filteredCharacters" :key="character.char_id">
 			<div class="card-image">
@@ -33,12 +44,20 @@ interface ICharacter {
 	birthday: string;
 	occupation: string[];
 	img: string;
-	status: string;
+	status: ECharacterStatus;
 	nickname: string;
-	appaerance: number[];
+	appearance: number[];
 	portrayed: string;
 	category: string;
 	better_call_saul_appearance: number[];
+}
+
+enum ECharacterStatus {
+	Alive = 'Alive',
+	Deceased = 'Deceased',
+	PresumeDead = 'Presumed dead',
+	Unknow = '?',
+	All = '',
 }
 
 export default Vue.extend({
@@ -52,12 +71,18 @@ export default Vue.extend({
     return {
       characters: [] as ICharacter[],
 	    nameFilter: '' as string,
+			statusesList: ECharacterStatus,
+			statusFilter: ECharacterStatus.All as ECharacterStatus,
+			seasonsNumber: 5 as number,
+	    seasonFilter: 0,
     }
   },
 	computed: {
   	filteredCharacters(): ICharacter[] {
   		return this.characters.filter(character => {
-  			return character.name.toLowerCase().includes(this.nameFilter.toLowerCase());
+  			return character.name.toLowerCase().includes(this.nameFilter.toLowerCase())
+					&& character.status.includes(this.statusFilter)
+					&& (character.appearance.includes(this.seasonFilter) || !this.seasonFilter);
 			})
 		}
 	}
